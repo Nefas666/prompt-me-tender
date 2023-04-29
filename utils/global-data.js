7ad2e7f2a1319622190202bb513e5986847a276a
s.manno <selene.manno@digitouch.it>
2023-04-30 01:59:09 +0200

    feat(Travel app): :fire: change of the repo actual purpose into a travel helper app

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,10 +1,10 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : '🧑‍✈️Take that f***ing flight🛫, Selene🪂!';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Fly like an eagle 🦅';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
