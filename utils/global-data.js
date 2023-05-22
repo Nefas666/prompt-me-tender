15a485e17cd8dd6e810306d5e2e8a94f8b40410e
s.manno <selene.manno@digitouch.it>
2023-05-22 12:18:50 +0200

    fix(Travel app): :wrench: update global-data

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,15 +1,15 @@
 export const getGlobalData = () => {
-  const name = process.env.local.BLOG_NAME
-    ? decodeURI(process.env.local.BLOG_NAME)
+  const name = process.env.BLOG_NAME
+    ? decodeURI(process.env.BLOG_NAME)
     : '🪂';
   const blogTitle = process.env.BLOG_TITLE
-    ? decodeURI(process.env.local.BLOG_TITLE)
+    ? decodeURI(process.env.BLOG_TITLE)
     : '🪂 ESCAPISM 🪂';
-  const blogDescription = process.env.local.BLOG_DESCRIPTION
-    ? decodeURI(process.env.local.BLOG_DESCRIPTION)
+  const blogDescription = process.env.BLOG_DESCRIPTION
+    ? decodeURI(process.env.BLOG_DESCRIPTION)
     : 'An app to help you make a decision about where to travel based on weather, finance and distance.';
-  const footerText = process.env.local.BLOG_FOOTER_TEXT
-    ? decodeURI(process.env.local.BLOG_FOOTER_TEXT)
+  const footerText = process.env.BLOG_FOOTER_TEXT
+    ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
 
   return {
