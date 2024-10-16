9c29580fb50ceb9f29212cda329ee88215aca737
Selene Manno <selenepuntoemme@gmail.com>
2024-10-16 23:00:04 +0200

    docs(dependencies): :alien: introduce shadcn and update package.json and related config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,29 @@ module.exports = {
     fontFamily: {
       mono: ['IBM Plex Mono', 'monospace', 'ui-monospace', 'SFMono-Regular'],
     },
+    extend: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
   },
 
   mode: 'jit',
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   darkMode: 'class', // or 'media' or 'class'
   presets: [require('./utils/tailwind-preset')],
