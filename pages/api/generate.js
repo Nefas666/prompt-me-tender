cbe7d17e2160ffcedc3f4f937fa2f57f44466582
Selene Manno <selenepuntoemme@gmail.com>
2024-10-16 15:28:38 +0200

    feat: :sparkles: set up prompt generation with GoogleAPI

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,6 +1,4 @@
-
-import axios from 'axios';
-
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
@@ -8,41 +6,22 @@ export default async function handler(req, res) {
     const prompt = `Suggest 3 perfect destinations for a trip in ${timeOfYear}, with a budget of ${priceRange}, lasting ${numberOfDays}, choosing ${typeOfTransport} to move?`;
 
     try {
-      const response = await axios.post(
-        'https://api.openai.com/v1/engines/text-davinci-003/completions',
-        {
-          prompt,
-          max_tokens: 256,
-          top_p: 1,
-          frequency_penalty: 0,
-          presence_penalty: 0,
-        },
-        {
-          headers: {
-            'Authorization': `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY}`,
-            'Content-Type': 'application/json',
-          },
-        }
-      );
+      const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GOOGLE_GEMINI_API_KEY);
+      const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-      const generatedText = response.data.choices[0].text.trim();
+      const result = await model.generateContent(prompt);
+      const generatedText = result.response.text(); 
 
       res.status(200).json({ generatedText });
     } catch (error) {
-      if (error.response) {
-              console.error(error.response.status, error.response.data);
-              res.status(error.response.status).json(error.response.data);
-            } else {
-              console.error(`Error with OpenAI API request: ${error.message}`);
-              res.status(500).json({
-                error: {
-                  message: 'An error occurred during your request.',
-                },
-              });
-            }
+      console.error(`Error with Google Gemini API request: ${error.message}`);
+      res.status(500).json({
+        error: {
+          message: 'An error occurred during your request.',
+        },
+      });
     }
   } else {
     res.status(404).json({ error: 'Invalid request method.' });
   }
 }
-
